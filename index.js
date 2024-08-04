e4a1a50c6bb6b1db54f93e94e0f1c329b87b29cb
workshop <Kamonsangmanee.w@gmail.com>
2024-08-05 01:09:34 +0700

    update commit

--- index.js
+++ index.js
@@ -1,192 +1,199 @@
-var product = [{
+const product = [{
     id: 1,
-    img: 'https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHNob2VzfGVufDB8fDB8fHww',
-    name: 'Vans',
-    price: 5000, // เปลี่ยนจาก 'print' เป็น 'price'
-    description: 'Vans Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam voluptate provident facilis impedit accusantium suscipit rem nobis, vel at, quibusdam est. Maxime quisquam nostrum adipisci nesciunt! Accusamus ipsum quibusdam porro!',
-    type: 'Shoes'
+    img: 'https://images.unsplash.com/photo-1491553895911-0055eca6402d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c2hvZXN8ZW58MHx8MHx8fDA%3D',
+    name: "Nike",
+    price: 4500,
+    description: 'Lorem iaudantium harum doloremque alias. Quae, vel ipsum quasi, voluptas,it optio nemo magni numquam non dicta voluptates porro! Vero eveniet numquam sit aut veleligendi officiis iste tenetur expedita. Delectus vero quibusdam adipisci in. Esses',
+    type: 'shoet'
+
 }, {
     id: 2,
-    img: 'https://media.istockphoto.com/id/932170554/photo/white-color-formal-shirt-with-button-down-collar-isolated-on-white.webp?b=1&s=170667a&w=0&k=20&c=yh4bZaGJa0gDXKEeIVxOVb2ONantdlozjqZEVlTQdmY=',
-    name: 'Shirt',
-    price: 1500, // เปลี่ยนจาก 'print' เป็น 'price'
-    description: 'shirt Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam voluptate provident facilis impedit accusantium suscipit rem nobis, vel at, quibusdam est. Maxime quisquam nostrum adipisci nesciunt! Accusamus ipsum quibusdam porro!',
-    type: 'Shirt'
+    img: 'https://media.istockphoto.com/id/483960103/th/%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%96%E0%B9%88%E0%B8%B2%E0%B8%A2/%E0%B8%94%E0%B9%89%E0%B8%B2%E0%B8%99%E0%B8%AB%E0%B8%99%E0%B9%89%E0%B8%B2%E0%B9%80%E0%B8%AA%E0%B8%B7%E0%B9%89%E0%B8%AD%E0%B8%A2%E0%B8%B7%E0%B8%94%E0%B8%AA%E0%B8%B5%E0%B8%94%E0%B9%8D%E0%B8%B2%E0%B9%80%E0%B8%9B%E0%B8%A5%E0%B9%88%E0%B8%B2%E0%B8%9E%E0%B8%A3%E0%B9%89%E0%B8%AD%E0%B8%A1%E0%B9%80%E0%B8%AA%E0%B9%89%E0%B8%99%E0%B8%97%E0%B8%B2%E0%B8%87%E0%B8%95%E0%B8%B1%E0%B8%94.jpg?s=2048x2048&w=is&k=20&c=mnY0KDHT_vEbNAraxX7O69WpQUkVZWT8KribHBollYo=',
+    name: "Black",
+    price: 600,
+    description: 'Lorem iaudantium harum doloremque alias. Quae, vel ipsum quasi, voluptas,it optio nemo magni numquam non dicta voluptates porro! Vero eveniet numquam sit aut veleligendi officiis iste tenetur expedita. Delectus vero quibusdam adipisci in. Esses',
+    type: 'shirt'
 }, {
     id: 3,
-    img: 'https://images.unsplash.com/photo-1511556532299-8f662fc26c06?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    name: 'Nike',
-    price: 4500, // เปลี่ยนจาก 'print' เป็น 'price'
-    description: 'Nike Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam voluptate provident facilis impedit accusantium suscipit rem nobis, vel at, quibusdam est. Maxime quisquam nostrum adipisci nesciunt! Accusamus ipsum quibusdam porro!',
-    type: 'Shoes'
+    img: 'https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHNob2VzfGVufDB8fDB8fHww',
+    name: "Vans",
+    price: 7000,
+    description: 'Lorem iaudantium harum doloremque alias. Quae, vel ipsum quasi, voluptas,it optio nemo magni numquam non dicta voluptates porro! Vero eveniet numquam sit aut veleligendi officiis iste tenetur expedita. Delectus vero quibusdam adipisci in. Esses',
+    type: 'shoet'
 }];
 
-
-//แสดง Product หน้าเว็บ
 $(document).ready(() => {
     var html = "";
     for (let i = 0; i < product.length; i++) {
-        html += `<div onclick="openProductDetail(${i})" class="product-item ${product[i].type}">
-                    <img src="${product[i].img}" class="product-img">
+        html += `<div onclick="openProductDetail(${i})" class="product-items ${product[i].type}" >
+                    <img class="product-img"
+                        src="${product[i].img}"alt="">
                     <p style="font-size: 1.2vw;">${product[i].name}</p>
-                    <p style="font-size: 1.2vw;">${numbercomams(product[i].price)}</p>
-                </div>`;
+                    <p stlye="font-size: 1vw;">${numberWithCommas(product[i].price)}</p>
+                </div>`
     }
-    $("#productlist").html(html); //ประกาศ Id
-});
+    $("#productlist").html(html)
+})
 
-function numbercomams(x) {
+function numberWithCommas(x) {
     x = x.toString();
     var pattern = /(-?\d+)(\d{3})/;
     while (pattern.test(x))
-        x = x.replace(pattern, "$1,$2")
+        x = x.replace(pattern, "$1,$2");
     return x;
 }
 
-function searchSomething(elem) {
-
-    var value = $('#' + elem.id).val()
-    console.log(value);
-
+function search(elem) {
+    
+    var value = $('#'+elem.id).val()
+    console.log(value)
     var html = "";
+
     for (let i = 0; i < product.length; i++) {
-        if (product[i].name.includes(value)) {
-            html += `<div onclick="openProductDetail(${i})" class="product-item ${product[i].type}">
-                        <img src="${product[i].img}" class="product-img">
-                        <p style="font-size: 1.2vw;">${product[i].name}</p>
-                        <p style="font-size: 1.2vw;">${numbercomams(product[i].price)}</p>
-                    </div>`;
+        if ( product[i].name.includes(value)) {
+        html += `<div onclick="openProductDetail(${i})" class="product-items ${product[i].type}" >
+                    <img class="product-img"
+                        src="${product[i].img}"alt="">
+                    <p style="font-size: 1.2vw;">${product[i].name}</p>
+                    <p style="font-size: 1vw;">${product[i].price}</p>
+                </div>`
+        }
+        if (html == '') {
+            $("#productlist").html(`<p> Not fond product</p>`)
+        } else {
+            $("#productlist").html(html)
         }
     }
-    if (html == '') {
-        $('#productlist').html(`<p>Not found product </p>`);
-    }
-
-    $("#productlist").html(html); //ประกาศ Id
-}
+} 
 
 function searchProduct(param) {
-    console.log(param);
-    $('.product-item').css(`display`, 'none')
-    if (param == 'all') {
-        $('.product-item').css(`display`, 'block')
+    $(".product-items").css('display', 'none')
+    if(param == 'all'){
+        $(".product-items").css('display', 'block')
     }
     else {
-        $("." + param).css(`display`, 'block')
+        $("."+param).css('display', 'block')
     }
 }
 
-var productIndex = 0;
+var productindex = 0;
 function openProductDetail(index) {
-    productIndex = index;
-    console.log(productIndex)
-    $('#modalDesc').css(`display`, 'flex')
-    $("#mdd-img").attr('src', product[index].img);
-    $("#mdd-name").text(product[index].name);
-    $("#mdd-price").text(numbercomams(product[index].price) + 'THB');
-    $("#mdd-desc").text(numbercomams(product[index].description));
+    productindex = index;
+    console.log(productindex)
+    $("#modalDec").css("display", "flex")
+    $("#mdd-img").attr('src', product[index].img)
+    $("#mdd-name").text(product[index].name)
+    $("#mdd-price").text(numberWithCommas(product[index].price) + 'THB')
+    $("#desc").text(product[index].description)
 }
 
 function closeModal() {
-    $(".modal").css(`display`, 'none')
+    $(".modal").css('display','none')
 }
 
 var cart = [];
-function addtocarts() {
+function addtocart() {
     var pass = true;
+
     for (let i = 0; i < cart.length; i++) {
-        if (productIndex == cart[i].index) {
+        if( productindex == cart[i].index ) {
+            console.log('found same product')
             cart[i].count++;
             pass = false;
         }
     }
-    if (pass) {
+
+    if(pass) {
         var obj = {
-            index: productIndex,
-            id: product[productIndex].id,
-            name: product[productIndex].name,
-            price: product[productIndex].price,
-            img: product[productIndex].img,
+            index: productindex,
+            id: product[productindex].id,
+            name: product[productindex].name,
+            price: product[productindex].price,
+            img: product[productindex].img,
             count: 1
-        }
-        console.log(obj);
+        };
+        // console.log(obj)
         cart.push(obj)
     }
-    console.log(cart);
+    console.log(cart)
+
     Swal.fire({
         icon: 'success',
-        title: "Add " + product[productIndex].name + ' to cart !',
-        timer: 1000
+        title: 'Add ' + product[productindex].name + ' to cart !'
     })
-    $("#cartcount").css(`display`, 'flex').text(cart.length)
-
+    $("#cartcount").css('display','flex').text(cart.length)
 }
 
-function opencarts() {
-    $(`#modalCart`).css(`display`, 'flex')
+function openCart() {
+    $('#modalCart').css('display','flex')
     rendercart();
 }
 
 function rendercart() {
-    if (cart.length > 0) {
+    if(cart.length > 0) {
         var html = '';
         for (let i = 0; i < cart.length; i++) {
-            html += `<div class="cartlist-item">
+            html += `<div class="cartlist-items">
                         <div class="cartlist-left">
-                            <img src="${cart[i].img}" >
-                            <div class="cartslist-detail">
+                            <img src="${cart[i].img}" alt="">
+                            <div class="cartlist-detail">
                                 <p style="font-size: 1.5vw;">${cart[i].name}</p>
-                                <p style="font-size: 1.2vw;">${numbercomams(cart[i].price * cart[i].count)}</p>
+                                <p style="font-size: 1.2vw;">${ numberWithCommas(cart[i].price * cart[i].count) } THB</p>
                             </div>
                         </div>
-                        <div class="cartlist-rigth">
+                        <div class="cartlist-right">
                             <p onclick="deinitems('-', ${i})" class="btnc">-</p>
-                            <p id=""countitems${i} style="margin: 0 20px;" >${cart[i].count}</p>
-                            <p onclick="deinitems('+', ${i})class="btnc">+</p>
+                            <p id="countitems${i}" style="margin: 0 20px;">${cart[i].count}</p>
+                            <p onclick="deinitems('+', ${i})" class="btnc">+</p>
                         </div>
                     </div>`;
         }
         $("#mycart").html(html)
     }
     else {
-        $("#mycart").html(`<p>Not found product</p>`)
+        $("#mycart").html(`<p>Not found product list</p>`)
     }
 }
 
 function deinitems(action, index) {
-    if (action == '-') {
-        if (cart[index].count > 0) {
+    if(action == '-') {
+        if(cart[index].count > 0) {
             cart[index].count--;
-            $('#countitems' + index).text(cart[index].count)
+            $("#countitems"+index).text(cart[index].count)
 
-            if (cart[index].count <= 0) {
+            if(cart[index].count <= 0) {
                 Swal.fire({
                     icon: 'warning',
                     title: 'Are you sure to delete?',
                     showConfirmButton: true,
+                    showCancelButton: true,
                     confirmButtonText: 'Delete',
-                    cancleButtonText: 'cancle'
+                    cancelButtonText: 'Cancel'
                 }).then((res) => {
-                    if (res.isConfirmed) {
-                        cart.splice(index, 1)
-                        rendercart();
-                        $("#cartcount").css(`display`, 'flex').text(cart.length)
-
-                        if(cart.length <= 0) {
-                            $("#cartcount").css(`display`, 'none').text(cart.length)
-                        }
-                        
-                    }
-                    else {
-                        cart[index].count++;
-                        $("#cartcount").css(`display`, 'flex').text(cart.length)
-
-                    }
+                  if(res.isConfirmed) {
+                     cart.splice(index, 1) 
+                     console.log(cart)
+                     rendercart();
+                     $("#cartcount").css('display','flex').text(cart.length)
+                     
+                     if(cart.length <= 0) {
+                        $("#cartcount").css('display','none')
+                     }
+                  }  
+                  else {
+                    cart[index].count++;
+                    $("#countitems"+index).text(cart[index].count)
+                    rendercart();
+                  }
                 })
             }
+            rendercart();
         }
+        
     }
-    else if (action == '+') {
-
+    else if(action == '+') {
+        cart[index].count++;
+        $("#countitems"+index).text(cart[index].count)
+        rendercart();
     }
 }
